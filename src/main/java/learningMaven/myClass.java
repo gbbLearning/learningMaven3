4acf6147b9b1fdba37b5473cf8619ce5de36ea5e
Gonzalo Benítez Bueno <gonsama@MacBook-Pro.home>
2020-04-12 17:21:06 +0200

    connected to database

--- src/main/java/learningMaven/myClass.java
+++ src/main/java/learningMaven/myClass.java
@@ -1,10 +1,31 @@
 package learningMaven;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class myClass {
 
     public void main()
     {
+         try{
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testing","postgres", "");
+            
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from probando.tabla");
+            while(resultSet.next())
+            {
+                System.out.println(resultSet.getString("columna"));
+            }
+
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Something went wrong, maybe pwd not set");
+            System.out.println(ex.getMessage());
+        }
         //App.main(null);
-        System.out.println("hola vscoder gilipollas!");
+        //System.out.println("hola vscoder gilipollas!");
     }
 }
